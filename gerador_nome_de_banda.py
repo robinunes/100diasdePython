be6d902635fb04d50e62cdf71c7b9f153e71b1f5
Robson Marcelo Diniz Nunes <110443501+robinunes@users.noreply.github.com>
2022-11-07 17:41:57 -0300

    Add files via upload

---
+++ gerador_nome_de_banda.py
@@ -0,0 +1,5 @@
+print ("Saudações amante da música! Hora de definir o nome da sua banda, para isto, me responda algumas perguntas")
+cidade = input ("Qual o nome da cidade que você cresceu? \n")
+pet = input ("Ótimo! Agora me diz: Qual o nome do Pet que mais marcou a sua vida? \n")
+banda = cidade + ' '+  pet
+print ("Que tal o nome da sua banda ser:", banda)
